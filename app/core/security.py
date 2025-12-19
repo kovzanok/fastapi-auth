c79e1691840a9d1995c835996df28f8028406172
kovzanok <alexanderkovzan@gmail.com>
2025-12-19 12:37:24 +0300

    feat: add security util functions

---
+++ app/core/security.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException, status
+import jwt
+from pwdlib import PasswordHash
+from datetime import datetime, timedelta
+
+from app.core.config import settings
+
+def create_token(data: dict, expire_minutes: int):
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=expire_minutes)
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, settings.SECRET_KEY, settings.ALGORITHM)
+
+def get_email_from_token(token: str) -> str:
+    payload = jwt.decode(token, settings.SECRET_KEY,algorithms=[settings.ALGORITHM])
+    email: str = payload.get("sub")
+
+    if not email:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    
+    return email
+
+password_hash = PasswordHash.recommended()
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return password_hash.verify(password, hashed_password)
+
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
