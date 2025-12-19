06dc39abadb83c8c4ce86b2e4dcc4eab550046f7
kovzanok <alexanderkovzan@gmail.com>
2025-12-19 12:07:20 +0300

    fix: make email column unique and index

--- app/db/models.py
+++ app/db/models.py
@@ -6,7 +6,7 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
-    email: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True, index=True)
     password: Mapped[str] = mapped_column(String, nullable=False)
     role: Mapped[str] = mapped_column(String, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
