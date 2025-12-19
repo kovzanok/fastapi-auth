01e6e91da6eeb854a35f29a9ee40d78f3eb9428d
kovzanok <alexanderkovzan@gmail.com>
2025-12-19 12:04:21 +0300

    fix: change user table columns

--- app/db/models.py
+++ app/db/models.py
@@ -1,13 +1,12 @@
 from sqlalchemy import BigInteger, String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.dialects.postgresql import ARRAY
 from app.db.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
-    email: Mapped[str] = String(40)
-    password: Mapped[str]
-    roles: Mapped[list[str]] = mapped_column(ARRAY(String), default=list, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    role: Mapped[str] = mapped_column(String, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
